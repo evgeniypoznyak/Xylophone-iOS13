844636075adda1f9d419f3ed2b55b35235137e0e
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-12-20 23:51:39 -0600

    fix the bug with no sound on iPhone

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,13 +18,38 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
         playSound(soundName: sender.currentTitle)
+        UIView.animate(withDuration: 0.2, delay: 0, options: UIView.AnimationOptions.curveEaseOut, animations: {
+            sender.alpha = 1
+        }, completion: nil)
     }
+    
+    
 
     func playSound(soundName: String?) {
-        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
-        player = try! AVAudioPlayer(contentsOf: url!)
-        player.play()
+//        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+//        player = try! AVAudioPlayer(contentsOf: url!)
+//        player.play()
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else { return }
+
+               do {
+                   try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                   try AVAudioSession.sharedInstance().setActive(true)
+
+                   /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+                   player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+                   /* iOS 10 and earlier require the following line:
+                   player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+                   guard let player = player else { return }
+
+                   player.play()
+
+               } catch let error {
+                   print(error.localizedDescription)
+               }
     }
 }
 
